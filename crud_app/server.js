9c691b1a414c23099a8e873d2be19ada640ead17
rick <rikktrevino@gmail.com>
2022-12-12 20:58:40 -0600

    Connect to MongoDB

--- crud_app/server.js
+++ crud_app/server.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
-const { response } = require('express');
+
+const connectDB = require('./server/database/connection');
 
 const app = express();
 
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 8080;
 
 // log requests
 app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({extended : true}));
