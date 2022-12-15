7a02a734edcc3b84f4b6506b7154601b31cb55f7
rick <rikktrevino@gmail.com>
2022-12-14 19:23:31 -0600

    API initialization. create/find/update/delete

---
+++ crud_app/server/controller/controller.js
@@ -0,0 +1,21 @@
+var  Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req, res) => {
+
+}
+
+// retrieve and return all users or retrieve and return a single user.
+exports.find = (req, res) => {
+    
+}
+
+// update a user by user id
+exports.update = (req, res) => {
+    
+}
+
+// delete a user by user id
+exports.delete = (req, res) => {
+    
+}
