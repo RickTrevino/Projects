79928f1bd289d5e5a71a41c2d9f7f1c1834e8a3b
rick <rikktrevino@gmail.com>
2022-12-17 18:28:10 -0600

    API/ find, update, delete + postman tested.

--- crud_app/server/controller/controller.js
+++ crud_app/server/controller/controller.js
@@ -23,22 +23,57 @@ exports.create = (req, res) => {
         })
         .catch(err => {
             res.status(500).send({
-                message: err.message || "An error occured while saving the new user to the database"
-            });
+                message: err.message || "An error occured while saving the new user to the database"});
         });
 }
 
 // retrieve and return all users or retrieve and return a single user.
 exports.find = (req, res) => {
-    
+   Userdb.find()
+        .then(user => {
+            res.send(user)
+        })
+        .catch(err => {
+            res.status(500).send({message:err.message || "An error occured while retrieving user information"});
+        });
 }
 
 // update a user by user id
 exports.update = (req, res) => {
-    
+    // validate body
+    if(!req.body){
+        return res.status(400)    
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body,{useFindAndModify:false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({message:`Cannot update user with the : ${id}. User not found.`});
+            }
+            else{
+                res.send(data);
+            }
+        })
+        .catch(err => {
+            res.status(500).send({message:"Error updating user information."});
+        });
 }
 
 // delete a user by user id
 exports.delete = (req, res) => {
+    const id = req.params.id;
     
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(400).send({message:`Cannot delete the user with id: ${id}. Please double check the user id and try again.`});
+            }
+            else{
+                res.send({message:"User was deleted successfully."});
+            }
+        })
+        .catch(err => {
+            res.status(500).send({message:"Could not delete user with id: " + id});
+        });
 }
