79b619dedbea0fc04113f22f54f3851fbd0aeef6
rick <rikktrevino@gmail.com>
2022-12-18 19:03:27 -0600

    update_user form now pulls and updates data entry

--- crud_app/server/services/render.js
+++ crud_app/server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a GET request to api/users
-    axios.get("http://localhost:3000/api/users")
+    axios.get('http://localhost:3000/api/users')
         .then(function(response){
             res.render('index', {users: response.data  });
         })
@@ -18,5 +18,11 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    res.render('update_user');
+    axios.get('http://localhost:3000/api/users', {params : {id : req.query.id}})
+        .then(function(userdata){
+            res.render("update_user", {user: userdata.data});
+        })
+        .catch(err => {
+            res.send(err);
+        });
 }
